2bd8da176cd3a0e658346f620e8b9dea95a5b8b7
huzaifasheikh2002 <huzaifanadeem621@gmail.com>
2022-09-20 01:51:19 +0500

    ok

---
+++ src/Componets/UseEffect-PClass.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const UseEffect = () => {
+const [name ,setName]=useState("huzaifa")
+const foo=()=>{
+      console.log("useEffect console");
+};
+foo()
+ return (<>
+  <h1>{name}</h1>
+  <button onClick={()=>{
+        setName("Nadeem")
+  }}>StateUpdate</button>
+  </>)
+}
+
+export default UseEffect
